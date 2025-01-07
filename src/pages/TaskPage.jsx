2df4e6c6b27744fff9a66987beaaba6cbe44ec56
Bruno Sales Noleto de Oliveira <160500149+Brunosno@users.noreply.github.com>
2025-01-07 17:47:37 -0300

    Correção de responsividade

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -13,7 +13,7 @@ function TaskPage() {
   }
 
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <button onClick={() => onSeeTasks()} className="absolute left-0 text-slate-100">
